a943ebf88240f10ce09c98d1b327661dd539cc9c
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-11-21 20:50:41 +0700

    fix authMiddleware

--- app/utils/token.js
+++ app/utils/token.js
@@ -16,19 +16,21 @@ const generateToken = (id) => {
 
 const decodeToken = (token) => {
 	// Ade
-	// TODO: Decode JWT token dan kembalikan user id
-	// input: JWT token
-	// output: user id
 	try {
+		if (!SECRET_KEY) {
+			throw new Error("Secret key is not defined");
+		}
+
 		const result = jwt.verify(token, SECRET_KEY);
 
-		if (!result.id) {
-			throw new Error("Invalid token");
+		if (!result || !result.id) {
+			throw new Error("Invalid token or payload");
 		}
 
 		return result.id;
 	} catch (err) {
 		console.error("Error decoding token:", err.message);
+
 		return false;
 	}
 };
